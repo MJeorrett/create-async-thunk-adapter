aa74d0602be59ced817766630626f8be76a1c5ba
Matthew Jeorrett <matthewjeorrett@gmail.com>
2020-11-04 22:02:27 +0000

    remove redundant code

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,14 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import * as fieldSummariesSlice from './fieldSummaries';
-import * as stepSummariesSlice from './stepSummaries';
 
 export const store = configureStore({
   reducer: {
     [fieldSummariesSlice.name]: fieldSummariesSlice.reducer,
-    [stepSummariesSlice.name]: stepSummariesSlice.reducer,
   },
 });
 
 export type AppDispatch = typeof store.dispatch;
 export { useFieldSummaries } from './fieldSummaries';
-export { useStepSummaries } from './stepSummaries';
